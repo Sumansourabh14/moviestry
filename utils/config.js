c0c514c3e95bb532d6eba4ce0185d2431ba3fb02
Suman Sourabh <suman.sourabh@ansrsource.com>
2024-02-17 00:40:07 +0530

    added live backend url

--- utils/config.js
+++ utils/config.js
@@ -1,2 +1,2 @@
-export const API_URL = `http://localhost:8001`;
-// export const API_URL = `https://moviestry-backend.onrender.com`;
+// export const API_URL = `http://localhost:8001`;
+export const API_URL = `https://moviestry-backend.onrender.com`;

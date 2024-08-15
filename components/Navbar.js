366b5803e3739950dca61549a84389be25ccbe4b
Suman Sourabh <suman.sourabh@ansrsource.com>
2024-08-15 21:25:33 +0530

    updated sign up link color on navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = () => {
             </Link>
             <Link
               href="/signup"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              className="text-gray-800 bg-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
             >
               Sign Up
             </Link>

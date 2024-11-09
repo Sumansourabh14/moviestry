707f4560188ac704a3b9cd78e0e6280df79a4f5a
Suman Sourabh <sumsourabh14@gmail.com>
2024-11-09 20:34:28 +0530

    updated login and sign up form width

--- app/signup/page.js
+++ app/signup/page.js
@@ -55,26 +55,24 @@ const SignUp = () => {
     <>
       <section className="flex flex-col items-center justify-between p-24">
         <section className="flex flex-col md:flex-row py-14">
-          <section className="flex flex-col items-center gap-8">
-            <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold">
+          <section className="flex flex-col gap-8">
+            <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold text-center">
               Sign Up
             </h1>
-            <p className="text-gray-500 text-center">
+            <p className="text-gray-500 text-center max-w-[360px]">
               Join {siteTitle} to manage your movies and TV
             </p>
             {!!error.error && <FormError message={error.message} />}
-            <section>
-              <SignUpForm
-                name={name}
-                handleNameChange={handleNameChange}
-                email={email}
-                handleEmailChange={handleEmailChange}
-                password={password}
-                handlePasswordChange={handlePasswordChange}
-                handleSubmit={handleSignUp}
-                loading={loading}
-              />
-            </section>
+            <SignUpForm
+              name={name}
+              handleNameChange={handleNameChange}
+              email={email}
+              handleEmailChange={handleEmailChange}
+              password={password}
+              handlePasswordChange={handlePasswordChange}
+              handleSubmit={handleSignUp}
+              loading={loading}
+            />
             <AlreadyAccount
               text="Already have an account?"
               destination={`/login`}

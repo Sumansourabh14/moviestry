4e8b344b0374b1ec18e3b15edb69167ad00dbd60
Suman Sourabh <sumsourabh14@gmail.com>
2024-11-10 22:42:43 +0530

    added hover elements on card

--- app/movies/page.js
+++ app/movies/page.js
@@ -10,9 +10,15 @@ const Movies = () => {
       pageTitle={"Browse Movies"}
     >
       <section className="flex flex-col gap-12">
+        <MoviesRow title="Trending" endpoint={API_REQUESTS.trending} />
         <MoviesRow title="Now Playing" endpoint={API_REQUESTS.nowPlaying} />
+        <MoviesRow title="Top Rated" endpoint={API_REQUESTS.topRated} />
         <MoviesRow title="Sci-fi" endpoint={API_REQUESTS.sciFi} />
         <MoviesRow title="Drama" endpoint={API_REQUESTS.drama} />
+        <MoviesRow title="Adventure" endpoint={API_REQUESTS.adventure} />
+        <MoviesRow title="Comedy" endpoint={API_REQUESTS.comedy} />
+        <MoviesRow title="Horror" endpoint={API_REQUESTS.horror} />
+        <MoviesRow title="War" endpoint={API_REQUESTS.war} />
       </section>
     </PageLayout>
   );
--- components/cards/MediaCard.js
+++ components/cards/MediaCard.js
@@ -3,8 +3,8 @@ import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
 
 const MediaCard = ({ posterPath, title, releaseDate }) => {
   return (
-    <Card className="rounded-sm">
-      <CardHeader className="p-0 pb-2">
+    <Card className="relative rounded-sm overflow-hidden group">
+      <CardHeader className="p-0">
         <Image
           src={
             process.env.NEXT_PUBLIC_TMDB_IMAGE_BASE_URL + `/w500/${posterPath}`
@@ -15,7 +15,7 @@ const MediaCard = ({ posterPath, title, releaseDate }) => {
           sizes="100vw"
           style={{ width: "100%", height: "auto" }} // optional
         />
-        <CardTitle
+        {/* <CardTitle
           title={title}
           className="text-base text-center overflow-hidden whitespace-nowrap text-ellipsis px-2"
         >
@@ -29,8 +29,15 @@ const MediaCard = ({ posterPath, title, releaseDate }) => {
           <CardDescription className="text-sm text-center">
             Unreleased
           </CardDescription>
-        )}
+        )} */}
       </CardHeader>
+
+      <div className="absolute inset-0 bg-black bg-opacity-90 flex flex-col justify-center items-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-100 px-4">
+        <h3 className="text-lg font-bold text-center">{title}</h3>
+        <p className="text-sm">
+          {releaseDate ? new Date(releaseDate).getFullYear() : "Unreleased"}
+        </p>
+      </div>
     </Card>
   );
 };
